61e378d0f2388fca12071fbf0982ed2d048def3f
nunomsantos <nuno21nt@gmail.com>
2015-03-14 13:51:51 +0000

    Simple changes on code style

--- bowling.rb
+++ bowling.rb
@@ -6,8 +6,8 @@ class Bowling
   end
 
   def roll(pins)
-    raise "game over" if game_over?
-    raise "impossible play" if pins < 0 || pins > pins_standing
+    fail 'game over' if game_over?
+    fail 'impossible play' if pins < 0 || pins > pins_standing
 
     rolls[playing_roll] = pins
     if strike_on_regular_frame? || neither_strike_nor_spare_on_last_frame?
@@ -17,10 +17,10 @@ class Bowling
 
   def score
     points = 0
-    for frame in 0..9
-      if strike? (frame)
+    (0..9).each do |frame|
+      if strike?(frame)
         points += 10 + bonus_strike(frame)
-      elsif spare? (frame)
+      elsif spare?(frame)
         points += 10 + bonus_spare(frame)
       else
         points += frame_points(frame)
@@ -32,27 +32,27 @@ class Bowling
   private
 
   def bonus_spare(frame)
-    rolls[2*frame+2].to_i
+    rolls[2 * frame + 2].to_i
   end
 
   def bonus_strike(frame)
     if frame < 9
-      rolls[2*frame+2].to_i + if strike? (frame+1)
-                                rolls[2*frame+4].to_i
-                              else
-                                rolls[2*frame+3].to_i
-                              end
+      rolls[2 * frame + 2].to_i + if strike?(frame + 1)
+                                    rolls[2 * frame + 4].to_i
+                                  else
+                                    rolls[2 * frame + 3].to_i
+                                  end
     else  # tenth frame
-      rolls[2*frame+1].to_i + rolls[2*frame+2].to_i
+      rolls[2 * frame + 1].to_i + rolls[2 * frame + 2].to_i
     end
   end
 
   def frame_points(frame)
-    rolls[2*frame].to_i + rolls[2*frame +1].to_i
+    rolls[2 * frame].to_i + rolls[2 * frame + 1].to_i
   end
 
   def game_over?
-    rolls.find_index(nil) == nil
+    rolls.find_index(nil).nil?
   end
 
   def neither_strike_nor_spare_on_last_frame?
@@ -61,12 +61,12 @@ class Bowling
 
   def pins_standing
     if playing_roll <= 18
-      playing_roll.even? ? pins = 10 : pins = 10 - rolls[playing_roll-1]
+      playing_roll.even? ? pins = 10 : pins = 10 - rolls[playing_roll - 1]
     else  # tenth frame
-      if spare? (9) # 10 pins on 3rd ball if tenth frame was a spare
+      if spare?(9) # 10 pins on 3rd ball if tenth frame was a spare
         pins = 10
       else # 2nd and (eventual) 3rd balls both follow this rule in other occasions
-        rolls[playing_roll-1] == 10 ? pins = 10 : pins = 10 - rolls[playing_roll-1]
+        rolls[playing_roll - 1] == 10 ? pins = 10 : pins = 10 - rolls[playing_roll - 1]
       end
     end
     pins
@@ -76,15 +76,15 @@ class Bowling
     rolls.find_index(nil).to_i
   end
 
-  def spare? (frame)
-    !strike?(frame) && ( rolls[2*frame].to_i + rolls[2*frame+1].to_i == 10 )
+  def spare?(frame)
+    !strike?(frame) && (rolls[2 * frame].to_i + rolls[2 * frame + 1].to_i == 10)
   end
 
-  def strike? (frame)
-    rolls[2*frame].to_i == 10
+  def strike?(frame)
+    rolls[2 * frame].to_i == 10
   end
 
   def strike_on_regular_frame?
-    (playing_roll < 18) && strike?(playing_roll/2) && (!game_over?)
+    (playing_roll < 18) && strike?(playing_roll / 2) && (!game_over?)
   end
 end
