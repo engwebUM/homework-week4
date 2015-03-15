c43acc73fb1bcebdea60c85c3683f01270a2d44a
Ricardo28Costa <ricardocostapsp@hotmail.com>
2015-03-15 06:52:14 -0700

    clean codeV1

--- bowling.rb
+++ bowling.rb
@@ -2,42 +2,53 @@
     
 	def initialize
     	@launch = []
-    	@position =[]
-   	end
- 
-   	def roll(pins)
+      @frist_launch=0
+      @frames=[]
+  end
+
+ 	def roll(pins)
+    if pins>=0 && pins <=10
     	@launch << pins
-   	end
+    end
+ 	end
 
 	def score 
   		score=0
   		frame=0
-  		i=0
-  		j=0
-  		while frame<10 #Each match consists in 10 
-  			if spare?(i) #SPARE (/)
-        		score += 10 + @launch[i + 2]# 10 points more the score obtained in the next ball  
-        		i+= 2
-        		@position[frame]=score# each position has score.
-      	elsif strike?(i) #STRIKE (X)
-        		score += 10 + @launch[i + 1] + @launch[i + 2]#bonus strike - 10 points more score obtained in the next two balls
-        		i+=1
-        		@position[frame]=score
+      max_frame=10
+  		while frame<max_frame  
+  			if spare?
+        		score += 10 + bonus_spare
+        		@frist_launch+= 2
+      	elsif strike? 
+        		score += 10 + bonus_strike
+        		@frist_launch+=1
   			else
-  				score+=@launch[i]+@launch[i+1]
-  				i+=2
-  				@position[frame]=score
+  				score+=normal_frame
+  				@frist_launch+=2
   			end
   		frame+=1
     	end
     	score
 	end
 
-	def spare?(i)
- 		@launch[i] + @launch[i + 1] == 10
+	def spare?
+ 		@launch[@frist_launch] + @launch[@frist_launch + 1] == 10
 	end
-	
-	def strike?(i)
- 		@launch[i] == 10
+
+  def bonus_spare
+    @launch[@frist_launch + 2]  
+  end
+
+	def strike?
+ 		@launch[@frist_launch] == 10
  	end
+
+  def bonus_strike
+    @launch[@frist_launch + 1] + @launch[@frist_launch + 2]  
+  end
+
+  def normal_frame
+    @launch[@frist_launch]+@launch[@frist_launch+1]
+  end
 end	 
