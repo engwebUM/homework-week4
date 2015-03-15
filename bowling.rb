e9c834facccdc17b20d9b3a2cc2a01e09f7ae60e
eduardopinto <dudacya@gmail.com>
2015-03-15 16:43:17 +0000

    Clean Code version1, tests working & Removed some validations that seems not important

--- bowling.rb
+++ bowling.rb
@@ -41,99 +41,120 @@ class Bowling
     end
   end
 
+  def verifyStrikeBonus(pins)
+    if(@rolls[@currentlyRollNumber-2] == "X")
+      @rolls[@currentlyRollNumber-3] += pins
+    end
+  end
+
+  def verifyStrikeBonus2(pins)
+    if(@rolls[@currentlyRollNumber-2] == "X" && @rolls[@currentlyRollNumber-4] == "X")
+      @rolls[@currentlyRollNumber-5] += pins
+    end
+  end
+
+  def verifyStrikeBonus3(pins)
+    if(@rolls[@currentlyRollNumber-1] == "X" && @rolls[@currentlyRollNumber-3] == "X")
+      @rolls[@currentlyRollNumber-4] += pins
+    end
+  end
+
+  def verifyStrikeOrSpareBonus(pins)
+    if(@rolls[@currentlyRollNumber-1] == "/" || @rolls[@currentlyRollNumber-1] == "X")
+      @rolls[@currentlyRollNumber-2] += pins
+    end
+  end
+
+  def verifyStrikeBonusFrame19(pins)
+    if(@rolls[@currentlyRollNumber-2] == "X")
+      @rolls[@currentlyRollNumber-5] += pins
+    end
+    if(@rolls[18] == 10 && @rolls[@currentlyRollNumber-2] == "X")
+      @rolls[@currentlyRollNumber-3] += pins
+    end
+  end
+
+  def verifyDoubleStrike
+
+  end
+
+  def frameType(pins)
+    if(@currentlyRollNumber < 18)
+      normalRoll(pins)
+    elsif (@currentlyRollNumber == 18)
+      frame18(pins)
+    elsif (@currentlyRollNumber == 19)
+      frame19(pins)
+    elsif (@currentlyRollNumber == 20)
+      frame20(pins)
+    else
+      raise "No rolls left"
+    end
+  end
+
+  def normalRoll(pins)
+    if (@currentlyRollNumber.odd?)
+      oddFrame(pins)
+    else
+      evenFrame(pins)
+    end
+  end
+
+  def oddFrame(pins)
+    verifyStrikeBonus(pins)
+    verifyStrikeBonus2(pins)
+    secondTry(pins)
+  end
+
+  def evenFrame(pins)
+    verifyStrikeOrSpareBonus(pins)
+    verifyStrikeBonus3(pins)
+    firstTry(pins)
+  end
+
+  def frame18(pins)
+    verifyStrikeOrSpareBonus(pins)
+    @rolls[@currentlyRollNumber] = pins
+    @currentlyRollNumber += 1
+  end
+
+  def frame19(pins)
+    verifyStrikeBonusFrame19(pins)
+    @rolls[@currentlyRollNumber] = pins
+    if (@rolls[18] == 10)
+      @rolls[@currentlyRollNumber] = pins
+    elsif ((pins+@rolls[@currentlyRollNumber-1])<10)
+      @rolls[@currentlyRollNumber] = pins
+    elsif ((pins+@rolls[@currentlyRollNumber-1]) == 10)
+      @rolls[@currentlyRollNumber-1] = 10
+      @rolls[@currentlyRollNumber] = "/"
+    end
+    @currentlyRollNumber += 1
+  end
+
+  def frame20(pins)
+    if (@rolls[@currentlyRollNumber-1] == "/" || @rolls[@currentlyRollNumber-1] == 10 || @rolls[@currentlyRollNumber-2] == 10)
+      @rolls[@currentlyRollNumber] += pins
+      @currentlyRollNumber += 1
+    else
+      raise "You didn't spared/striked before, you can't play the 3rd ball"
+    end
+  end
+
+
   def roll(pins)
-      #roll number 19,20,21 are different than the others, has no bonus
-      if (@currentlyRollNumber < 18)
-        if (@currentlyRollNumber.odd?)
-          #adding bonus if striked on last roll
-          if(@rolls[@currentlyRollNumber-2] == "X")
-            @rolls[@currentlyRollNumber-3] += pins
-          end
-          #adding bonus if striked 2 times a row
-          if(@rolls[@currentlyRollNumber-2] == "X" && @rolls[@currentlyRollNumber-4] == "X")
-            @rolls[@currentlyRollNumber-5] += pins
-          end
-          #second try of each roll
-          secondTry(pins)
-        else
-          #adding bonus if striked or spared on last roll
-          if(@rolls[@currentlyRollNumber-1] == "/" || @rolls[@currentlyRollNumber-1] == "X")
-            @rolls[@currentlyRollNumber-2] += pins
-          end
-          #adding bonus if striked 2 times a row
-          if(@rolls[@currentlyRollNumber-1] == "X" && @rolls[@currentlyRollNumber-3] == "X")
-            @rolls[@currentlyRollNumber-4] += pins
-          end
-          #first try of each roll
-          firstTry(pins)
-        end
-      else
-        #last frame
-        #first roll
-        if (@currentlyRollNumber == 18)
-          #adding bonus if striked or spared on last roll
-          if(@rolls[@currentlyRollNumber-1] == "/" || @rolls[@currentlyRollNumber-1] == "X")
-            @rolls[@currentlyRollNumber-2] += pins
-          end
-          @rolls[@currentlyRollNumber] = pins
-          @currentlyRollNumber += 1
-        #second roll
-        elsif (@currentlyRollNumber == 19)
-          #adding bonus if striked on last roll
-          if(@rolls[@currentlyRollNumber-2] == "X")
-            @rolls[@currentlyRollNumber-5] += pins
-          end
-          #adding bonus if striked 2 times a row
-          if(@rolls[18] == 10 && @rolls[@currentlyRollNumber-2] == "X")
-            @rolls[@currentlyRollNumber-3] += pins
-          end
-          #if didnt spared/striked before
-          @rolls[@currentlyRollNumber] = pins
-          if (@rolls[18] == 10)
-            #strike
-            @rolls[@currentlyRollNumber] = pins
-          else
-            #normal case
-            #check if the number of pins of second roll try
-            #didn't reached the limit (10 pins for each roll)
-            if ((pins+@rolls[@currentlyRollNumber-1])<10)
-              @rolls[@currentlyRollNumber] = pins
-            else
-              if ((pins+@rolls[@currentlyRollNumber-1]) == 10)
-                #Spare case
-                @rolls[@currentlyRollNumber-1] = 10
-                @rolls[@currentlyRollNumber] = "/"
-              else
-                raise "Invalid number of pins"
-              end
-            end
-          end
-          @currentlyRollNumber += 1
-        #third roll
-        elsif (@currentlyRollNumber == 20)
-          #In case we do a spare, or 2 strikes a row, or just playing the last ball
-          if (@rolls[@currentlyRollNumber-1] == "/" || @rolls[@currentlyRollNumber-1] == 10 || @rolls[@currentlyRollNumber-2] == 10)
-            @rolls[@currentlyRollNumber] += pins
-            @currentlyRollNumber += 1
-          else
-            @currentlyRollNumber += 1
-            raise "You didn't spared/striked before, you can't play the 3rd ball"
-          end
-        elsif (@currentlyRollNumber > 20)
-          raise "No rolls left, game is over"
-        end
-      end
+    frameType(pins)
   end
 
   def score
     $score = 0
     for i in 0...21
-      $score += roll_score(i)
+      $score += rollScore(i)
     end
     return $score
   end
 
-  def roll_score(iteration)
+  def rollScore(iteration)
     $frame_score = @rolls[iteration]
     if($frame_score == "X" || $frame_score == "/")
       $frame_score = 0
